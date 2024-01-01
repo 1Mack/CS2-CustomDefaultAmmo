abbf9842470f978e3396a0ba7874725771039b9b
1MacK <maaaaaack@outlook.com>
2024-01-01 12:05:41 -0300

    add networkupdate and fix some things

--- CustomDefaultAmmo.cs
+++ CustomDefaultAmmo.cs
@@ -8,7 +8,7 @@ namespace CustomDefaultAmmo;
 
 public class CustomDefaultAmmoConfig : BasePluginConfig
 {
-  [JsonPropertyName("Weapons")] public string Weapons { get; set; } = "weapon_awp,11,-1";
+  [JsonPropertyName("Weapons")] public string Weapons { get; set; } = "weapon_awp,11,-1|weapon_deagle,-1,15";
 }
 public class CustomDefaultAmmo : BasePlugin, IPluginConfig<CustomDefaultAmmoConfig>
 {
@@ -37,35 +37,46 @@ public class CustomDefaultAmmo : BasePlugin, IPluginConfig<CustomDefaultAmmoConf
     {
       if (entity == null || entity.Entity == null || !entity.IsValid || !entity.DesignerName.Contains("weapon_")) return;
 
-      if (!Config.Weapons.Contains(entity.DesignerName)) return;
-
-
       foreach (var item in Config.Weapons.Split("|"))
       {
         string[] weaponValues = item.Split(",");
 
-        if (weaponValues.Length < 2 || weaponValues[0].Trim() != entity.DesignerName) return;
+        if (weaponValues.Length < 2 || weaponValues[0].Trim() != entity.DesignerName) continue;
 
         var weapon = new CBasePlayerWeapon(entity.Handle);
         Server.NextFrame(() =>
         {
-          try
-          {
-            if (!weapon.IsValid) return;
+          if (!weapon.IsValid) return;
 
-            CCSWeaponBaseVData? _weapon = weapon.As<CCSWeaponBase>().VData;
-            if (_weapon == null) return;
-            _weapon.MaxClip1 = int.Parse(weaponValues[1]);
-            _weapon.DefaultClip1 = int.Parse(weaponValues[1]);
-            if (weaponValues[2].Length > 0 && weaponValues[2] != "-1")
-              _weapon.PrimaryReserveAmmoMax = int.Parse(weaponValues[2]);
+          CCSWeaponBase _weapon = weapon.As<CCSWeaponBase>();
+          if (_weapon == null) return;
+
+          if (weaponValues[1] != "-1")
+          {
+            if (_weapon.VData != null)
+            {
+              _weapon.VData.MaxClip1 = int.Parse(weaponValues[1]);
+              _weapon.VData.DefaultClip1 = int.Parse(weaponValues[1]);
+            }
+
+            _weapon.Clip1 = int.Parse(weaponValues[1]);
+
+            Utilities.SetStateChanged(weapon.As<CCSWeaponBase>(), "CBasePlayerWeapon", "m_iClip1");
+
           }
-          catch (Exception) { }
+
+          if (weaponValues[2].Length > 0 && weaponValues[2] != "-1")
+          {
+            if (_weapon.VData != null)
+            {
+              _weapon.VData.PrimaryReserveAmmoMax = int.Parse(weaponValues[2]);
+            }
+            _weapon.ReserveAmmo[0] = int.Parse(weaponValues[2]);
+
+            Utilities.SetStateChanged(weapon.As<CCSWeaponBase>(), "CBasePlayerWeapon", "m_pReserveAmmo");
+          }
         });
       }
-
-
     });
-
   }
 }
