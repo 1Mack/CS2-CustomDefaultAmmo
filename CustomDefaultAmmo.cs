bd1191097a669674ff3ca8a7cf6df5a2e6e51274
1MacK <maaaaaack@outlook.com>
2024-01-02 09:22:05 -0300

    fix

--- CustomDefaultAmmo.cs
+++ CustomDefaultAmmo.cs
@@ -8,7 +8,7 @@ namespace CustomDefaultAmmo;
 
 public class CustomDefaultAmmoConfig : BasePluginConfig
 {
-  [JsonPropertyName("Weapons")] public string Weapons { get; set; } = "weapon_awp,11,-1|weapon_deagle,-1,15";
+  [JsonPropertyName("Weapons")] public string Weapons { get; set; } = "weapon_awp,10,-1|weapon_deagle,-1,15";
 }
 public class CustomDefaultAmmo : BasePlugin, IPluginConfig<CustomDefaultAmmoConfig>
 {
@@ -41,12 +41,20 @@ public class CustomDefaultAmmo : BasePlugin, IPluginConfig<CustomDefaultAmmoConf
       {
         string[] weaponValues = item.Split(",");
 
-        if (weaponValues.Length < 2 || weaponValues[0].Trim() != entity.DesignerName) continue;
+        if (weaponValues.Length < 2) continue;
 
-        var weapon = new CBasePlayerWeapon(entity.Handle);
+
         Server.NextFrame(() =>
         {
+
+          var weapon = new CBasePlayerWeapon(entity.Handle);
+
           if (!weapon.IsValid) return;
+
+
+          weaponValues[0] = weaponValues[0].Trim();
+
+          if (!checkIfWeapon(weaponValues[0], weapon.AttributeManager.Item.ItemDefinitionIndex)) return;
 
           CCSWeaponBase _weapon = weapon.As<CCSWeaponBase>();
           if (_weapon == null) return;
@@ -78,5 +86,50 @@ public class CustomDefaultAmmo : BasePlugin, IPluginConfig<CustomDefaultAmmoConf
         });
       }
     });
+
+    static bool checkIfWeapon(string weaponName, int weaponDefIndex)
+    {
+      Dictionary<int, string> WeaponDefindex = new()
+      {
+        { 1, "weapon_deagle" },
+        { 2, "weapon_elite" },
+        { 3, "weapon_fiveseven" },
+        { 4, "weapon_glock" },
+        { 7, "weapon_ak47" },
+        { 8, "weapon_aug" },
+        { 9, "weapon_awp" },
+        { 10, "weapon_famas" },
+        { 11, "weapon_g3sg1" },
+        { 13, "weapon_galilar" },
+        { 14, "weapon_m249" },
+        { 16, "weapon_m4a1" },
+        { 17, "weapon_mac10" },
+        { 19, "weapon_p90" },
+        { 23, "weapon_mp5sd" },
+        { 24, "weapon_ump45" },
+        { 25, "weapon_xm1014" },
+        { 26, "weapon_bizon" },
+        { 27, "weapon_mag7" },
+        { 28, "weapon_negev" },
+        { 29, "weapon_sawedoff" },
+        { 30, "weapon_tec9" },
+        { 32, "weapon_hkp2000" },
+        { 33, "weapon_mp7" },
+        { 34, "weapon_mp9" },
+        { 35, "weapon_nova" },
+        { 36, "weapon_p250" },
+        { 38, "weapon_scar20" },
+        { 39, "weapon_sg556" },
+        { 40, "weapon_ssg08" },
+        { 60, "weapon_m4a1_silencer" },
+        { 61, "weapon_usp_silencer" },
+        { 63, "weapon_cz75a" },
+        { 64, "weapon_revolver" },
+      };
+
+      if (WeaponDefindex.ContainsKey(weaponDefIndex) && WeaponDefindex[weaponDefIndex] == weaponName) return true;
+
+      return false;
+    }
   }
 }
